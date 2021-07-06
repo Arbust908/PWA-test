89e8700e1cc91a4fa8a689318416d70a9c4265fc
Fran Blanco <arbust908@gmail.com>
2021-07-06 12:48:11 -0300

    Feat::PWA-init

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,18 @@ import store from "@/store";
 import App from '@/App.vue';
 import "./index.css";
 
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // show a prompt to user
+  },
+  onOfflineReady() {
+    // show a ready to work offline to user
+  },
+})
+// when user clicked the "refresh" button
+updateSW()
+// the page will reload and the up-to-date content will be served.
+
 createApp(App).use(router).use(store).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({
+    registerType: 'autoUpdate',  
+  manifest: {
+    // content of manifest
+  },
+  workbox: {
+    // workbox options for generateSW
+  }
+  })],
   resolve: {
     alias: [
       { find: '@', replacement: '/src' }
