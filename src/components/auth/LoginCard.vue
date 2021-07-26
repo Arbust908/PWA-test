5526409e77cfdbaabf859516b05fe0c850e45240
Fran Blanco <arbust908@gmail.com>
2021-07-26 10:59:31 -0300

    Feat::Red

--- src/components/auth/LoginCard.vue
+++ src/components/auth/LoginCard.vue
@@ -99,15 +99,12 @@
               shadow-sm
               px-4
               py-2
-              bg-indigo-600
+              bg-red-600
               text-base
               font-medium
               text-second-50
-              hover:bg-indigo-700
-              focus:outline-none
-              focus:ring-2
-              focus:ring-offset-2
-              focus:ring-indigo-500
+              hover:bg-red-700
+              focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500
               sm:text-sm
             "
             @click.prevent="closeModal"
