f18c4289fb2aaf827bc0d017856e0e2672240e24
Fran Blanco <arbust908@gmail.com>
2021-07-30 05:22:12 -0300

    No more sandCenter

--- src/router/index.ts
+++ src/router/index.ts
@@ -133,24 +133,6 @@ const routes: Array<RouteRecordRaw> = [
     beforeEnter: isLogged,
   },
   {
-    path: '/centro-de-carga-de-arena',
-    name: 'SandCenters',
-    component: () => import('@/pages/SandCenter/Index.vue'),
-    beforeEnter: isLogged,
-  },
-  {
-    path: '/centro-de-carga-de-arena/:id',
-    name: 'SandCenterById',
-    component: () => import('@/pages/SandCenter/_id.vue'),
-    beforeEnter: isLogged,
-  },
-  {
-    path: '/centro-de-carga-de-arena/nueva',
-    name: 'NewSandCenter',
-    component: () => import('@/pages/SandCenter/New.vue'),
-    beforeEnter: isLogged,
-  },
-  {
     path: '/notificaciones-a-proveedores',
     name: 'ProviderNotifications',
     component: () => import('@/pages/ProviderNotification/Index.vue'),
