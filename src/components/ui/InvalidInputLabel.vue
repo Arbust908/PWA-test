7a7ace002ae6b9e3cf1a67ecdaff930a753a0cd9
Tomás Milgron <milgron@gmail.com>
2021-10-05 14:21:14 -0300

    Feat::SFC-230 Validation label implementation

---
+++ src/components/ui/InvalidInputLabel.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <span class="label-wrapper">
+      <div class="icon-wrapper">
+        <Icon icon="ExclamationCircle" type="solid" class="icon" />
+      </div>
+      <div class="message-wrapper">{{message}}</div>
+    </span>
+  </div>
+</template>
+
+<script>
+import { ref, toRefs, watchEffect } from 'vue'
+import Icon from '@/components/icon/TheAllIcon.vue';
+
+export default {
+  components: {
+    Icon
+  },
+  props: {
+    validationType: {
+      type: String,
+      required: false
+    }
+  },
+  setup (props) {
+    const validationType = ref(props.validationType)
+    const message = ref("")
+
+    watchEffect(() => {
+      if(validationType.value == "empty") message.value = "Este campo es obligatorio."
+      if(validationType.value == "email") message.value = "Formato de mail incorrecto."
+      if(validationType.value == "extension") message.value = "Cantidad de caracteres incorrecto."
+    })
+  
+    return {
+      message
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@keyframes enter {
+  from {opacity: 0;}
+  to {opacity: 1}
+}
+.label-wrapper {
+  animation: enter 500ms;
+  @apply flex mt-2 items-center;
+}
+.message-wrapper {
+  @apply text-red-700 ml-1;
+}
+.icon-wrapper {
+  .icon {
+    @apply h-5 w-5 text-red-700;
+  }
+}
+</style>
--- src/components/ui/form/FieldTextArea.vue
+++ src/components/ui/form/FieldTextArea.vue
@@ -3,25 +3,32 @@
     <FieldTitle v-if="title" :title="title" :isOptional="isOptional" />
     <textarea
       :id="fieldName"
-      :class="isFixed ? 'resize-none' : null"
+      :class="[isFixed ? 'resize-none' : null, !isValid && wasInputEntered ? 'invalid' : null]"
       class="input"
       :name="fieldName"
       :placeholder="placeholder"
       v-model="value"
       :rows="rows"
+      @blur="initiateValidations"
     >
     </textarea>
+    <InvalidInputLabel v-if="!isValid && wasInputEntered" :validationType="validationType"/>
   </label>
 </template>
 
 <script>
-  import { defineComponent, computed } from 'vue';
+  import { defineComponent, computed, toRefs, ref } from 'vue';
   import { useVModel } from '@vueuse/core';
   import FieldTitle from '@/components/ui/form/FieldTitle.vue';
+  import useVuelidate from '@vuelidate/core'
+  import {required, email} from '@vuelidate/validators'
+  import InvalidInputLabel from '../InvalidInputLabel.vue';
+
   export default defineComponent({
     name: 'FieldInput',
     components: {
       FieldTitle,
+      InvalidInputLabel
     },
     props: {
       data: {
@@ -51,12 +58,48 @@
         type: Boolean,
         default: false,
       },
+      requireValidation: {
+        type: Boolean,
+        require: false,
+      },
     },
     setup(props, { emit }) {
       const value = useVModel(props, 'data', emit);
+      const {requireValidation, fieldName} = toRefs(props)
+      const wasInputEntered = ref(false)
+      const validationType = ref(props.validationType || "empty")
+      const validationRules = ref({})
+      const state = {
+        [`${fieldName.value}`]: value
+      }
+      
+      if(requireValidation.value) {
+        validationRules.value = {
+          [`${fieldName.value}`]: {required}
+        }
+      }
+      
+      const v$ = useVuelidate(validationRules, state)
+
+      const initiateValidations = () => {
+        wasInputEntered.value = true
+      }
+
+      const isValid = computed(() => {
+        if(!wasInputEntered.value) return true
+        if(!v$.value.$silentErrors[0]) return true
+        return v$.value.$silentErrors[0].$propertyPath == fieldName ? true : false
+      })
+
       return {
         value,
         ...props,
+        v$,
+        fieldName,
+        isValid,
+        initiateValidations,
+        wasInputEntered,
+        validationType
       };
     },
   });
@@ -64,4 +107,11 @@
 
 <style lang="scss" scoped>
   @import '@/assets/inputs.scss';
+  .input:not(.invalid) {
+    transition: all 100ms ease-out;
+  }
+  .invalid {
+    transition: all 500ms ease-out;
+    @apply border border-red-500;
+  }
 </style>
