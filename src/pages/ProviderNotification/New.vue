dd5f65cd41b533500312fb9d798399149cbfd8a7
marcosiraola <iraolamarcos@gmail.com>
2021-10-15 16:43:37 -0300

    Fix::SFC-164 resize boton

--- src/pages/ProviderNotification/New.vue
+++ src/pages/ProviderNotification/New.vue
@@ -500,7 +500,7 @@
     @apply flex justify-center items-center rounded-md border border-black border-opacity-10 shadow-sm px-4 py-2 bg-white text-base font-medium text-second-400 hover:bg-gray-100 ;
   }
   .modal-create-new-button {
-    @apply inline-flex justify-center w-1/2 rounded-md border border-transparent shadow-sm px-4 py-2 bg-main-600 text-base font-medium text-second-50 hover:bg-main-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-main-500;
+    @apply inline-flex justify-center w-1/2 rounded-md border border-transparent shadow-sm px-4 py-3 bg-main-600 text-base font-medium text-second-50 hover:bg-main-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-main-500;
   }
   .input {
     @apply w-full px-3 py-2 rounded focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-second-300 mt-1 flex shadow-sm;
