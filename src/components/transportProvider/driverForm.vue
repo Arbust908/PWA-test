1755616c7840e871da7611146e11a7c8cdec101d
Tomás Milgron <milgron@gmail.com>
2021-10-18 17:05:58 -0300

    Feat::SFC-278 add driver observations in transport provider form

--- src/components/transportProvider/driverForm.vue
+++ src/components/transportProvider/driverForm.vue
@@ -1,157 +1,162 @@
 <template>
-  <FieldGroup>
-    <FieldInput
-      class="col-span-full"
-      title="Nombre y apellido"
-      fieldName="driverName"
-      placeholder="Ingresar conductor"
-      :data="driverName"
-      @update:data="driverName = $event"
-      requireValidation
-      :silenced="silenced"
-      entity="transportProvider"
-      @change="validationHandler"
-    />
-    <FieldInput
-      class="col-span-full"
-      fieldName="driverPhone"
-      placeholder="+11 1234 5678"
-      mask="#*"
-      title="Teléfono"
-      :data="driverPhone"
-      @update:data="driverPhone = $event"
-      requireValidation
-      :silenced="silenced"
-      entity="transportProvider"
-    />
-    <FieldInput
-      class="col-span-full"
-      fieldName="driverEmail"
-      placeholder="empresa@mail.com"
-      title="Email"
-      :data="driverEmail"
-      @update:data="driverEmail = $event"
-      requireValidation
-      :silenced="silenced"
-      entity="transportProvider"
-      validationType="email"
-    />
-    <FieldInput
-      class="col-span-full"
-      title="Tipo de transporte"
-      fieldName="driverVehicleType"
-      placeholder="Doble carga"
-      :data="driverTType"
-      @update:data="driverTType = $event"
-      requireValidation
-      :silenced="silenced"
-      entity="transportProvider"
-    />
-    <FieldInput
-      class="col-span-full"
-      title="Patente"
-      fieldName="driverVehicleId"
-      placeholder="patente"
-      :data="driverTId"
-      @update:data="driverTId = $event"
-      requireValidation
-      :silenced="silenced"
-      entity="transportProvider"
-    />
-    <button
-      :class="[
-        'flex items-center w-[250px]',
-        driverFull ? null : 'text-gray-200 cursor-not-allowed',
-      ]"
-      @click.prevent="driverFull && addDriver()"
-    >
-      <Icon icon="Plus" type="outline" class="w-5 h-5" />
-      <h2>Agregar Transportista</h2>
-    </button>
-  </FieldGroup>
+    <FieldGroup>
+        <FieldInput
+            class="col-span-full"
+            title="Nombre y apellido"
+            fieldName="driverName"
+            placeholder="Ingresar conductor"
+            :data="driverName"
+            @update:data="driverName = $event"
+            requireValidation
+            :silenced="silenced"
+            entity="transportProvider"
+            @change="validationHandler"
+        />
+        <FieldInput
+            class="col-span-full"
+            fieldName="driverPhone"
+            placeholder="+11 1234 5678"
+            mask="#*"
+            title="Teléfono"
+            :data="driverPhone"
+            @update:data="driverPhone = $event"
+            requireValidation
+            :silenced="silenced"
+            entity="transportProvider"
+        />
+        <FieldInput
+            class="col-span-full"
+            fieldName="driverEmail"
+            placeholder="empresa@mail.com"
+            title="Email"
+            :data="driverEmail"
+            @update:data="driverEmail = $event"
+            requireValidation
+            :silenced="silenced"
+            entity="transportProvider"
+            validationType="email"
+        />
+        <FieldInput
+            class="col-span-full"
+            title="Tipo de transporte"
+            fieldName="driverVehicleType"
+            placeholder="Doble carga"
+            :data="driverTType"
+            @update:data="driverTType = $event"
+            requireValidation
+            :silenced="silenced"
+            entity="transportProvider"
+        />
+        <FieldInput
+            class="col-span-full"
+            title="Patente"
+            fieldName="driverVehicleId"
+            placeholder="patente"
+            :data="driverTId"
+            @update:data="driverTId = $event"
+            requireValidation
+            :silenced="silenced"
+            entity="transportProvider"
+        />
+        <FieldTextArea
+            class="col-span-full"
+            fieldName="observations"
+            placeholder="Observaciones..."
+            title="Observaciones"
+            :rows="5"
+            isFixed
+            isOptional
+            :data="driverObs"
+            @update:data="driverObs = $event"
+        />
+        <button
+            :class="['flex items-center w-[250px]', driverFull ? null : 'text-gray-200 cursor-not-allowed']"
+            @click.prevent="driverFull && addDriver()"
+        >
+            <Icon icon="Plus" type="outline" class="w-5 h-5" />
+            <h2>Agregar Transportista</h2>
+        </button>
+    </FieldGroup>
 </template>
 
 <script lang="ts">
-  import { defineComponent, ref, computed } from 'vue';
-  import FieldGroup from '@/components/ui/form/FieldGroup.vue';
-  import FieldInput from '@/components/ui/form/FieldInput.vue';
-  import { useVModels } from '@vueuse/core';
-  import Icon from '@/components/icon/TheAllIcon.vue';
+    import { defineComponent, ref, computed } from 'vue';
+    import FieldGroup from '@/components/ui/form/FieldGroup.vue';
+    import FieldInput from '@/components/ui/form/FieldInput.vue';
+    import FieldTextArea from '@/components/ui/form/FieldTextArea.vue';
+    import { useVModels } from '@vueuse/core';
+    import Icon from '@/components/icon/TheAllIcon.vue';
 
-  export default defineComponent({
-    props: {
-      driverName: {
-        type: String,
-        default: '',
-      },
-      driverPhone: {
-        type: String,
-        default: '123456789',
-      },
-      driverEmail: {
-        type: String,
-        default: '',
-      },
-      driverTType: {
-        type: String,
-        default: '',
-      },
-      driverTId: {
-        type: String,
-        default: '',
-      },
-      driverObs: {
-        type: String,
-        default: '',
-      }
-    },
-    components: {
-      FieldGroup,
-      FieldInput,
-      Icon
-    },
-    setup(props, { emit }) {
-      const {
-        driverName,
-        driverPhone,
-        driverEmail,
-        driverTType,
-        driverTId,
-        driverObs
-      } = useVModels(props, emit);
+    export default defineComponent({
+        props: {
+            driverName: {
+                type: String,
+                default: '',
+            },
+            driverPhone: {
+                type: String,
+                default: '123456789',
+            },
+            driverEmail: {
+                type: String,
+                default: '',
+            },
+            driverTType: {
+                type: String,
+                default: '',
+            },
+            driverTId: {
+                type: String,
+                default: '',
+            },
+            driverObs: {
+                type: String,
+                default: '',
+            },
+        },
+        components: {
+            FieldGroup,
+            FieldInput,
+            FieldTextArea,
+            Icon,
+        },
+        setup(props, { emit }) {
+            const { driverName, driverPhone, driverEmail, driverTType, driverTId, driverObs } = useVModels(props, emit);
 
-      const silenced = ref(true)
+            const silenced = ref(true);
 
-      const validationHandler = () => {
-        silenced.value = false
-      }
+            const validationHandler = () => {
+                silenced.value = false;
+            };
 
-      const addDriver = () => {
-        silenced.value = true
-        emit('add-driver')
-      }
+            const addDriver = () => {
+                silenced.value = true;
+                emit('add-driver');
+            };
 
-      const driverFull: ComputedRef<boolean> = computed(() => {
-        return driverName.value !== '' &&
-        driverPhone.value !== '' &&
-        driverEmail.value !== '' &&
-        driverTType.value !== '' &&
-        driverTId.value !== ''
-      });
+            const driverFull: ComputedRef<boolean> = computed(() => {
+                return (
+                    driverName.value !== '' &&
+                    driverPhone.value !== '' &&
+                    driverEmail.value !== '' &&
+                    driverTType.value !== '' &&
+                    driverTId.value !== ''
+                );
+            });
 
-      return {
-        driverName,
-        driverPhone,
-        driverEmail,
-        driverTType,
-        driverTId,
-        driverObs,
-        silenced,
-        validationHandler,
-        driverFull,
-        addDriver,
-        Icon
-      };
-    },
-  });
+            return {
+                driverName,
+                driverPhone,
+                driverEmail,
+                driverTType,
+                driverTId,
+                driverObs,
+                silenced,
+                validationHandler,
+                driverFull,
+                addDriver,
+                Icon,
+            };
+        },
+    });
 </script>
