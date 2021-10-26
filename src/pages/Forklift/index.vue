6f29e95deb6630a01c9bdaa7a381dd9b6b81d40b
Matias Mancilla <matias.mancilla@bitpatagonia.com>
2021-10-26 18:51:26 -0300

    Fix styling in table column

--- src/pages/Forklift/index.vue
+++ src/pages/Forklift/index.vue
@@ -26,7 +26,7 @@
                 <GhostBtn size="sm" @click="clearFilters()"> Borrar filtros </GhostBtn>
             </div>
         </div>
-        <UiTable class="mt-5">
+        <UiTable class="mt-5 lg:w-7/12 min-w-min">
             <template #header>
                 <tr>
                     <th v-for="column in tableColumns" :key="column.name" :class="column.class" scope="col">
@@ -51,7 +51,7 @@
                     <td :class="f.name ? null : 'empty'">
                         {{ f.name || 'Sin nombre' }}
                     </td>
-                    <td :class="f.observations ? null : 'empty'">
+                    <td>
                         <p class="w-52 truncate">
                             {{ f.observations || 'Sin observaciones' }}
                         </p>
