e1cd6c59bb87cd84b851d80e9542531b57da690a
Fran Blanco <arbust908@gmail.com>
2021-11-16 17:25:05 -0300

    Feat::SFC-345 Transportista Nuevas patentes

---
+++ src/components/ui/buttons/SideBtn.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <BaseBtn :btn="`side ${btn}`" :type="type" :size="size" :disabled="disabled" :is-loading="isLoading">
+            <slot></slot>
+        </BaseBtn>
+    </div>
+</template>
+
+<script>
+    import BaseBtn from '@/components/ui/buttons/BaseBtn.vue';
+    export default {
+        components: {
+            BaseBtn,
+        },
+        props: {
+            type: {
+                type: String,
+                default: 'button',
+            },
+            size: {
+                type: String,
+                default: 'md',
+            },
+            // btn Classes
+            btn: {
+                type: String,
+                default: 'main',
+            },
+            disabled: {
+                type: String,
+                default: '',
+            },
+            isLoading: {
+                type: Boolean,
+                default: false,
+            },
+        },
+    };
+</script>
+
+<style lang="scss" scoped>
+    @import '@/assets/button.scss';
+</style>
