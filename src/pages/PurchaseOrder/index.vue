c49985fae1db6e2f03728a996e82ed59220e0a49
Matias Mancilla <matias.mancilla@bitpatagonia.com>
2021-11-19 14:29:18 -0300

    WIP

--- src/pages/PurchaseOrder/index.vue
+++ src/pages/PurchaseOrder/index.vue
@@ -1,118 +1,159 @@
 <template>
     <Layout>
-        <header class="flex justify-between items-center mb-4">
-            <h2 class="text-2xl font-semibold text-gray-900">Ordenes de Pedido</h2>
+        <header class="flex justify-start space-x-4 items-center mb-4">
+            <h2 class="text-2xl font-semibold text-gray-900">Orden de Pedido</h2>
             <router-link to="/orden-de-pedido/nueva">
-                <PrimaryBtn>Crear nueva</PrimaryBtn>
+                <PrimaryBtn size="sm">
+                    <span>Crear</span>
+                    <Icon icon="PlusCircle" class="ml-1 w-4 h-4" />
+                </PrimaryBtn>
             </router-link>
         </header>
-        <UiTable>
-            <template #header>
-                <tr>
-                    <th scope="col" title="Numero de Pedido">N°</th>
-                    <th scope="col">Centro de Carga de Arena</th>
-                    <th scope="col">Proveedor de transporte</th>
-                    <!-- <th scope="col">Estado</th> -->
-                    <th scope="col">
-                        <span class="sr-only">Acciones</span>
-                    </th>
-                </tr>
+        <hr />
+
+        <div class="relative grid grid-cols-12 col-span-full gap-4 mt-2">
+            <FieldSelect
+                title="Filtro"
+                class="col-span-full sm:col-span-5 md:col-span-3 lg:col-span-4 xl:col-span-3"
+                field-name="name"
+                placeholder="Seleccionar cliente"
+                endpoint="/company"
+                :data="clientId"
+                @update:data="clientId = $event"
+            />
+            <div class="col-span-full sm:mt-7 sm:col-span-5">
+                <GhostBtn size="sm" @click="clientId.value = -1"> Borrar filtros </GhostBtn>
+            </div>
+        </div>
+
+        <VTable class="mt-5" :columns="columns" :pagination="pagination" :items="purchaseOrders" :actions="actions">
+            <template #item="{ item: pO }">
+                <!-- Desktop -->
+                <td>
+                    {{ pO.id }}
+                </td>
+                <td>
+                    {{ (pO.company && pO.company.name) || '-' }}
+                </td>
+                <td>
+                    {{ (pO.pit && pO.pit.name) || '-' }}
+                </td>
+                <td>
+                    {{ (pO.sandProvider && pO.sandProvider.name) || '-' }}
+                </td>
+                <td>
+                    {{ (pO.transportProvider && pO.transportProvider.name) || '-' }}
+                </td>
+                <td>
+                    <Badge text="Enviada" classes="bg-green-600 text-white px-8 rounded-lg" />
+                </td>
             </template>
-            <template #body>
-                <tr
-                    v-for="(pO, poKey) in PurchaseOrders"
-                    :key="pO.id"
-                    :class="poKey % 2 === 0 ? 'even' : 'odd'"
-                    class="body-row"
-                >
-                    <td>
-                        {{ pO.id }}
-                    </td>
-                    <td :class="pO.sandProvider ? null : 'empty'">
-                        {{ (pO.sandProvider && pO.sandProvider.name) || 'Sin proveedor' }}
-                    </td>
-                    <!-- <td :class="pO.sandOrder ? null : 'empty'">
-            {{ pO.sandOrder.amount }}
-          </td> -->
-                    <td :class="pO.transportProvider ? null : 'empty'">
-                        {{ (pO.transportProvider && pO.transportProvider.name) || 'Sin proveedor' }}
-                    </td>
-                    <td>
-                        <div class="btn-panel">
-                            <!-- <router-link :to="`/orden-de-pedido/${pO.id}`" class="edit">
-                <Icon icon="PencilAlt" class="w-5 h-5" />
-                <span> Editar </span>
-              </router-link> -->
-                            <button class="delete" @click="deletePO(pO.id)">
-                                <Icon icon="Trash" class="w-5 h-5" />
-                                <span> Eliminar </span>
-                            </button>
-                        </div>
-                    </td>
-                </tr>
-                <tr v-if="PurchaseOrders.length <= 0">
-                    <td colspan="5" class="emptyState">
-                        <p>No hay Ordenes de Pedidos</p>
-                    </td>
-                </tr>
+
+            <!-- Mobile -->
+            <template #mobileTitle="{ item }">
+                {{ item.company.name }}
             </template>
-        </UiTable>
+
+            <template #mobileSubtitle="{ item }"> <span class="font-bold">Remito: </span>{{ item.id }} </template>
+        </VTable>
     </Layout>
 </template>
 
 <script lang="ts">
-    import { ref, watch } from 'vue';
+    import { ref, onMounted } from 'vue';
     import { createNamespacedHelpers } from 'vuex-composition-helpers';
-    const { useState } = createNamespacedHelpers('purchaseOrder');
-    import { useActions } from 'vuex-composition-helpers';
+    import { useStore } from 'vuex';
+    import { useRouter } from 'vue-router';
+    import { useStoreLogic } from '@/helpers/useStoreLogic';
     import Layout from '@/layouts/Main.vue';
     import PrimaryBtn from '@/components/ui/buttons/PrimaryBtn.vue';
-    import UiTable from '@/components/ui/TableWrapper.vue';
+    import GhostBtn from '@/components/ui/buttons/GhostBtn.vue';
+    import VTable from '@/components/ui/table/VTable.vue';
+    import FieldSelect from '@/components/ui/form/FieldSelect.vue';
+    import Badge from '@/components/ui/Badge.vue';
     import Icon from '@/components/icon/TheAllIcon.vue';
     import { useTitle, useMagicKeys, whenever } from '@vueuse/core';
-
-    import { PurchaseOrder } from '@/interfaces/sandflow.Types';
-    import { useApi } from '@/helpers/useApi';
 
     export default {
         components: {
             PrimaryBtn,
+            GhostBtn,
             Layout,
-            UiTable,
+            VTable,
             Icon,
+            Badge,
+            FieldSelect,
         },
         setup() {
             useTitle('Ordenes de Pedido <> Sandflow');
+            const store = useStore();
+            const router = useRouter();
+            const clientId = ref(-1);
+            const purchaseOrders = ref([]);
 
-            const { Ctrl_C } = useMagicKeys();
-            whenever(Ctrl_C, () => {
-                console.log('Crear Nuevo!');
+            const pagination = ref({
+                sortKey: 'id',
+                sortDir: 'asc',
+                // currentPage: 1,
+                // perPage: 10,
             });
 
-            const { deletePurchaseOrder, savePurchaseOrder } = useActions(['deletePurchaseOrder', 'savePurchaseOrder']);
-            const { read, destroy } = useApi('/purchaseOrder');
-            const PurchaseOrders = read();
-            watch(PurchaseOrders, (newValue, _) => {
-                if (newValue) {
-                    storeToState(newValue);
+            const columns = [
+                { title: 'Remito', key: 'id', sortable: true },
+                { title: 'Cliente', key: 'company.name', sortable: true },
+                { title: 'Pozo', key: 'pit.name', sortable: true },
+                { title: 'Prov.Arena', key: 'sandProvider.name', sortable: true },
+                { title: 'Transporte', key: 'transportProvider.name', sortable: true },
+                { title: 'Estado', key: 'operadora' },
+                { title: '', key: 'name' },
+            ];
+
+            const actions = [
+                {
+                    label: 'Ver más',
+                    onlyMobile: true,
+                    callback: () => {
+                        console.log('Ver más');
+                    },
+                },
+                {
+                    label: 'Reenviar',
+                    callback: (item) => {
+                        console.log('Reenviar');
+                    },
+                },
+                {
+                    label: 'Imprimir',
+                    callback: (item) => {
+                        console.log('Imprimir');
+                    },
+                },
+                {
+                    label: 'Cancelar',
+                    callback: (item) => {
+                        console.log('Cancelar');
+                    },
+                },
+            ];
+
+            const getPurchaseOrders = async () => {
+                const result = await useStoreLogic(router, store, 'purchaseOrder', 'getAll');
+
+                if (result.type == 'success') {
+                    purchaseOrders.value = result.res;
                 }
+            };
+
+            onMounted(async () => {
+                getPurchaseOrders();
             });
-            const storeToState = (pOs: PurchaseOrder[]) => {
-                return pOs.map((pO) => {
-                    savePurchaseOrder(pO);
-                });
-            };
-            const deletePO = async (poId: number) => {
-                PurchaseOrders.value = PurchaseOrders.value.filter((pO: PurchaseOrder) => {
-                    return pO.id !== poId;
-                });
-                const data = destroy(poId);
-                deletePurchaseOrder(poId);
-            };
 
             return {
-                PurchaseOrders,
-                deletePO,
+                purchaseOrders,
+                clientId,
+                pagination,
+                columns,
+                actions,
             };
         },
     };
