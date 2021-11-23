e4233a353e2cbc9f673be690c15abd81cc7f4703
Fran Blanco <arbust908@gmail.com>
2021-11-23 18:48:14 -0300

    Feat::SFC-348 Form + Transport Card

--- src/pages/TransportProvider/_id.vue
+++ src/pages/TransportProvider/_id.vue
@@ -5,9 +5,9 @@
                 Proveedor de transporte - ID {{ id }}
             </h1>
         </header>
-        <section class="flex">
-            <section class="w-8/12">
-                <nav class="flex justify-between max-w-2xl bg-white">
+        <section class="flex flex-wrap md:flex-nowrap">
+            <section class="w-full md:w-8/12">
+                <nav v-if="!showDrivers" class="flex justify-between max-w-2xl bg-white">
                     <button
                         :class="['section-tab', activeSection === 'provider' ? 'active' : '']"
                         :selected="activeSection === 'provider'"
@@ -41,7 +41,10 @@
                         @update:crEmail="companyRepresentative.email = $event"
                     />
                 </section>
-                <section v-if="activeSection === 'driver'" class="bg-white rounded-md max-w-2xl shadow-sm">
+                <section
+                    v-if="activeSection === 'driver' && !showDrivers"
+                    class="bg-white rounded-md max-w-2xl shadow-sm"
+                >
                     <form method="POST" action="/" class="p-4 max-w-lg">
                         <TransportProviderDriverForm
                             :driver-name="newDriver.name"
@@ -60,18 +63,74 @@
                         />
                     </form>
                 </section>
+                <section v-if="showDrivers" class="w-full md:w-4/12 mt-12 flex flex-col gap-y-4 md:hidden">
+                    <label class="input flex" for="search">
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                fill-rule="evenodd"
+                                clip-rule="evenodd"
+                                d="M6 2C3.79086 2 2 3.79086 2 6C2 8.20914 3.79086 10 6 10C8.20914 10 10 8.20914 10 6C10 3.79086 8.20914 2 6 2ZM0 6C0 2.68629 2.68629 0 6 0C9.31371 0 12 2.68629 12 6C12 7.29583 11.5892 8.49572 10.8907 9.47653L15.7071 14.2929C16.0976 14.6834 16.0976 15.3166 15.7071 15.7071C15.3166 16.0976 14.6834 16.0976 14.2929 15.7071L9.47653 10.8907C8.49572 11.5892 7.29583 12 6 12C2.68629 12 0 9.31371 0 6Z"
+                                fill="#9CA3AF"
+                            />
+                        </svg>
+                        <input
+                            id="search"
+                            v-model="searchDriver"
+                            class="search-input"
+                            type="text"
+                            placeholder="Buscar"
+                        />
+                    </label>
+                    <button class="flex items-center w-[250px] gap-2 py-2" @click.prevent="toggleDriverShown">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M12 0C5.37097 0 0 5.37097 0 12C0 18.629 5.37097 24 12 24C18.629 24 24 18.629 24 12C24 5.37097 18.629 0 12 0ZM18.9677 13.3548C18.9677 13.6742 18.7065 13.9355 18.3871 13.9355H13.9355V18.3871C13.9355 18.7065 13.6742 18.9677 13.3548 18.9677H10.6452C10.3258 18.9677 10.0645 18.7065 10.0645 18.3871V13.9355H5.6129C5.29355 13.9355 5.03226 13.6742 5.03226 13.3548V10.6452C5.03226 10.3258 5.29355 10.0645 5.6129 10.0645H10.0645V5.6129C10.0645 5.29355 10.3258 5.03226 10.6452 5.03226H13.3548C13.6742 5.03226 13.9355 5.29355 13.9355 5.6129V10.0645H18.3871C18.7065 10.0645 18.9677 10.3258 18.9677 10.6452V13.3548Z"
+                                fill="#8DC881"
+                            />
+                        </svg>
+                        <h2 class="font-semibold">Agregar Transportista</h2>
+                    </button>
+                    <DriverCard
+                        v-for="(driver, index) in filteredDrivers"
+                        :key="index"
+                        :name="driver.name"
+                        :phone="driver.phone"
+                        :email="driver.email"
+                        :vehicle-type="driver.vehicleType"
+                        :transport-id="driver.transportId"
+                        :transport-id2="driver.transportId2"
+                        :observations="driver.observations"
+                        @delete-driver="deleteDriver(index)"
+                        @edit-driver="editDriver(index)"
+                    />
+                </section>
                 <footer class="mt-[32px] gap-3 flex flex-col md:flex-row justify-end max-w-2xl">
-                    <section class="w-full space-x-3 flex items-center justify-end">
+                    <SideBtn
+                        v-if="activeSection === 'driver' && drivers.length"
+                        class="md:hidden"
+                        btn="full"
+                        @click="toggleDriverShown"
+                    >
+                        {{ showDrivers ? 'Volver' : driverTabText }}
+                    </SideBtn>
+                    <section v-if="!showDrivers" class="w-full space-x-3 flex items-center justify-end">
                         <SecondaryBtn btn="wide" @click.prevent="$router.push('/proveedores-de-transporte')">
                             Cancelar
                         </SecondaryBtn>
-                        <PrimaryBtn btn="wide" :disabled="!isFull ? 'yes' : null" @click="isFull && update()">
+                        <PrimaryBtn
+                            btn="wide"
+                            :disabled="!isFull ? 'yes' : null"
+                            @click="
+                                hasFullNewDriver && addDriver();
+                                isValidated && save();
+                            "
+                        >
                             Finalizar
                         </PrimaryBtn>
                     </section>
                 </footer>
             </section>
-            <section class="w-4/12 mt-12 ml-4 flex flex-col gap-y-4">
+            <section class="hidden w-full md:w-4/12 mt-12 ml-4 md:flex flex-col gap-y-4">
                 <DriverCard
                     v-for="(driver, index) in drivers"
                     :key="index"
@@ -80,6 +139,7 @@
                     :email="driver.email"
                     :vehicle-type="driver.vehicleType"
                     :transport-id="driver.transportId"
+                    :transport-id2="driver.transportId2"
                     :observations="driver.observations"
                     @delete-driver="deleteDriver(index)"
                     @edit-driver="editDriver(index)"
@@ -93,14 +153,14 @@
     import { computed, reactive, ref } from 'vue';
     import { useStore } from 'vuex';
     import { useRouter, useRoute } from 'vue-router';
-    import { useTitle } from '@vueuse/core';
-    import Icon from '@/components/icon/TheAllIcon.vue';
+    import { useTitle, useBreakpoints, breakpointsTailwind } from '@vueuse/core';
+
     import TransportProviderFrom from '@/components/transportProvider/providerForm.vue';
     import TransportProviderDriverForm from '@/components/transportProvider/driverForm.vue';
     import DriverCard from '@/components/transportProvider/DriverCard.vue';
     import Layout from '@/layouts/Main.vue';
     import SecondaryBtn from '@/components/ui/buttons/SecondaryBtn.vue';
-    import CircularBtn from '@/components/ui/buttons/CircularBtn.vue';
+    import SideBtn from '@/components/ui/buttons/SideBtn.vue';
     import PrimaryBtn from '@/components/ui/buttons/PrimaryBtn.vue';
     // AXIOS
     import axios from 'axios';
@@ -111,14 +171,13 @@
 
     export default {
         components: {
-            CircularBtn,
             DriverCard,
-            Icon,
             Layout,
             SecondaryBtn,
             PrimaryBtn,
             TransportProviderFrom,
             TransportProviderDriverForm,
+            SideBtn,
         },
         setup() {
             const store = useStore();
@@ -136,7 +195,6 @@
             const currentTransportProvider: TransportProvider = transportProviders.find((sp) => {
                 return sp.id == id;
             });
-            console.log('ctp', currentTransportProvider);
 
             let activeSection = ref('provider');
 
@@ -144,9 +202,27 @@
                 return (activeSection.value = option);
             };
 
-            console.log(currentTransportProvider.drivers);
+            const driversShown = ref(false);
+            const toggleDriverShown = () => {
+                return (driversShown.value = !driversShown.value);
+            };
+            const breakpoints = useBreakpoints(breakpointsTailwind);
+            const maxMD = breakpoints.smaller('md');
+            const showDrivers = computed(() => {
+                console.log(maxMD.value);
+
+                return driversShown.value && maxMD.value;
+            });
+
             const drivers: Array<Driver> = reactive(currentTransportProvider.drivers);
-            console.log(drivers);
+
+            const searchDriver = ref('');
+
+            const filteredDrivers = computed(() => {
+                return drivers.filter((driver) => {
+                    return driver.name.toLowerCase().includes(searchDriver.value.toLowerCase());
+                });
+            });
 
             let newDriver = reactive({
                 name: '',
@@ -167,7 +243,6 @@
             };
 
             const deleteDriver = async (index: number) => {
-                console.log(drivers[index].id);
                 let response = await axios
                     .delete(`${api}/driver/${drivers[index].id}`)
                     .then((res) => {
@@ -222,7 +297,6 @@
                 observations: currentTransportProvider.observations,
                 companyRepresentativeId: currentTransportProvider.companyRepresentativeId,
             });
-            console.log(newTransportProvider.legalId);
 
             const companyRepresentative: CompanyRepresentative = reactive({
                 name: currentTransportProvider.companyRepresentative.name,
@@ -260,14 +334,15 @@
                 return transportProviderFull.value && repFull.value;
             });
 
+            const driverTabText = computed(() => {
+                return `Transportista${drivers.length > 1 ? `s (${drivers.length})` : ''}`;
+            });
+
             const update = async () => {
                 if (hasFullNewDriver.value) {
                     addDriver();
                 }
                 const { drivers, ...newTProv } = currentTransportProvider;
-
-                console.log('newTraPro', newTransportProvider);
-                console.log('newComRep', companyRepresentative);
 
                 const { data: tpData } = useAxios(
                     `/transportProvider/${id}`,
@@ -281,12 +356,10 @@
                 );
                 drivers.forEach((driver) => {
                     if (driver.id) {
-                        console.log('driver tiene ID, y aexiste');
                         useAxios(`/driver/${driver.id}`, { method: 'PUT', data: driver }, instance);
                     } else {
                         const { id, ...newDriver } = driver;
                         newDriver.transportProviderId = currentTransportProvider.id;
-                        console.log('newDriver', newDriver);
                         const { data } = useAxios(`/driver/`, { method: 'POST', data: newDriver }, instance);
                     }
                 });
@@ -302,7 +375,6 @@
                 activeSection,
                 changeSection,
                 drivers,
-                Icon,
                 addDriver,
                 newDriver,
                 hasFullNewDriver,
@@ -311,12 +383,19 @@
                 id,
                 currentTransportProvider,
                 update,
+                driversShown,
+                showDrivers,
+                driverTabText,
+                toggleDriverShown,
+                searchDriver,
+                filteredDrivers,
             };
         },
     };
 </script>
 
 <style lang="scss" scoped>
+    @import '@/assets/inputs.scss';
     .btn {
         &__draft {
             @apply border-main-400 text-main-500 bg-transparent hover:bg-main-50 hover:shadow-lg;
@@ -365,5 +444,12 @@
     }
     .equip-grid {
         @apply grid gap-4 grid-cols-2 md:grid-cols-3;
+    }
+
+    .input {
+        @apply bg-white flex-row;
+    }
+    .search-input {
+        @apply bg-transparent border-none text-sm p-0 ml-2;
     }
 </style>
